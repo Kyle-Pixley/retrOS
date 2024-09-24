df04750aa91f4253779d747315d8a922238776da
KylePixley <kyle_pixley@hotmail.com>
2024-09-24 16:49:45 -0400

    calculator styling/ wip

--- src/components/calculator/calculatorLogic/CalculatorLogic.jsx
+++ src/components/calculator/calculatorLogic/CalculatorLogic.jsx
@@ -9,13 +9,14 @@ function CalculatorLogic() {
     const handleCalculatorButtonClick = (value) => {
         if ( value === '=') {
             setCalculatorResult(eval(calculatorInput));
-        } else if (value === 'C') {
-            setCalculatorInput('');
-            setCalculatorResult('');
         } else {
             setCalculatorInput(calculatorInput + value);
         }
     };
+    const handleCalculatorCButton = () => {
+        setCalculatorInput('');
+        setCalculatorResult('');
+    }
 
   return (
     <div id='calculator-logic-container'>
@@ -24,12 +25,16 @@ function CalculatorLogic() {
             <div id='calculator-result' className='calculator-input-result'>{calculatorResult}</div>
         </div>
 
-        <div id='calculator-buttons'>
-            {[ '7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '0', '.', '=', '+', 'C'].map((i) => ( 
-                <button key={i} onClick={() => handleCalculatorButtonClick(i)}>
-                    {i}
-                </button>
-            ))}
+        <div id='calculator-logic-buttons-container'>
+
+            <button id='calculator-c-button' className='calculator-buttons' onClick={() =>handleCalculatorCButton()}>C</button>
+            <div id='calculator-buttons-parent'>
+                {[ '7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '0', '.', '=', '+'].map((i) => ( 
+                    <button className='calculator-buttons' key={i} onClick={() => handleCalculatorButtonClick(i)}>
+                        {i}
+                    </button>
+                ))}
+            </div>
         </div>
     </div>
   )
