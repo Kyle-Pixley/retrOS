4cbd9a018bd3f6558825c8e80922f595db54c84b
Kyle-Pixley <kyle_pixley@hotmail.com>
2026-02-07 11:11:52 -0500

    hook order fix

--- retrOS/src/components/hooks/isMobile.jsx
+++ retrOS/src/components/hooks/isMobile.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
 //finds out if user is on a screen 799px wide or smaller for mobile accessibility
-function isMobile() {
+export default function isMobile() {
     const [ userIsMobile, setUserIsMobile ] = useState(false);
 
     useEffect(() => {
